e1350862390133034cc4c33af95637b0db0415d4
arefhosseini <arefhosseni@yahoo.com>
2021-11-10 13:18:45 +0330

    remote helper added

--- app/src/main/java/com/fearefull/moviefetcher/data/remote/NetworkDataSource.kt
+++ app/src/main/java/com/fearefull/moviefetcher/data/remote/NetworkDataSource.kt
@@ -1,8 +1,28 @@
 package com.fearefull.moviefetcher.data.remote
 
+import com.fearefull.moviefetcher.core.Resource
+import com.fearefull.moviefetcher.data.model.Movie
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
 /**
  * Created by Aref Hosseini on ۱۰/۱۱/۲۰۲۱.
  */
 
-class NetworkDataSource {
+@ExperimentalCoroutinesApi
+class NetworkDataSource @Inject constructor(
+    private val webService: WebService
+) {
+    suspend fun getMovieByImdbID(imdbID: String): Flow<Resource<Movie>> =
+        callbackFlow {
+            trySend(
+                Resource.Success(
+                    webService.getMovieByImdbID(imdbID, "") //TODO: add apikey
+                )
+            )
+            awaitClose { close() }
+        }
 }
--- app/src/main/java/com/fearefull/moviefetcher/data/remote/WebService.kt
+++ app/src/main/java/com/fearefull/moviefetcher/data/remote/WebService.kt
@@ -1,8 +1,15 @@
 package com.fearefull.moviefetcher.data.remote
 
+import com.fearefull.moviefetcher.data.model.Movie
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * Created by Aref Hosseini on ۱۰/۱۱/۲۰۲۱.
  */
 
 interface WebService {
+    @GET("URL")
+    suspend fun getMovieByImdbID(@Query(value = "i") imdbID: String,
+                                 @Query(value = "apikey") apiKey: String): Movie
 }
