358d77d30a9f5d0c7bd759a36e1ab6298d87bd54
arefhosseini <arefhosseni@yahoo.com>
2021-11-10 13:17:59 +0330

    app db

--- app/src/main/java/com/fearefull/moviefetcher/data/local/AppDatabase.kt
+++ app/src/main/java/com/fearefull/moviefetcher/data/local/AppDatabase.kt
@@ -1,8 +1,14 @@
 package com.fearefull.moviefetcher.data.local
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.fearefull.moviefetcher.data.model.FavoriteMovieEntity
+
 /**
  * Created by Aref Hosseini on ۱۰/۱۱/۲۰۲۱.
  */
 
-class AppDatabase {
+@Database(entities = [FavoriteMovieEntity::class],version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
 }
